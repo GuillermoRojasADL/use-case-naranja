446de901d2937760d6afe8b6988e772e138a06bd
pablo-andrade <pablo_andrade@mckinsey.com>
2019-06-28 15:45:28 -0500

    Write h5 as utf-8

--- 02-Python-code/00-ReadTextFiles.py
+++ 02-Python-code/00-ReadTextFiles.py
@@ -1,20 +1,21 @@
 import pandas as pd
-pd.options.mode.chained_assignment = None  # Disable annoying pandas warning
+import sys
 
-# archivos_clientes = \
-#     [
-#     # 'sbgr1_ps_bc',
-#     'sbgr1_ps_bo_role',
-#     # 'sbgr1_ps_por_habdata_tbl',
-#     # 'sbgr1_ps_por_salotr_tbl',
-#     # 'sbgr1_ps_aa_rd_person',
-#     'sbgr1_ps_bo_cm',
-#     'sbgr1_ps_cm',
-#     #'sbgr1_ps_por_inf_com_tbl',
-#     #'sbgr1_ps_rd_person']
+pd.options.mode.chained_assignment = None  # Disable annoying pandas warning
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
 
 archivos_clientes = \
-    ['sbgr1_ps_bo_role','sbgr1_ps_bo_cm','sbgr1_ps_cm']
+    ['sbgr1_ps_bc',
+     'sbgr1_ps_por_habdata_tbl',
+     'sbgr1_ps_por_salotr_tbl',
+     'sbgr1_ps_aa_rd_person',
+     'sbgr1_ps_por_inf_com_tbl',
+     'sbgr1_ps_rd_person',
+     'sbgr1_ps_bo_role',
+     'sbgr1_ps_bo_cm',
+     'sbgr1_ps_cm']
 
 INPUT_EXTENSION = 'txt'
 OUT_EXTENSION = 'h5'
@@ -22,25 +23,13 @@ OUT_EXTENSION = 'h5'
 INPUT_DATA_PATH = '/mnt/s3-refined-porvenir/Clientes/'
 OUTPUT_DATA_PATH = '/mnt/s3-refined-porvenir/Clientes/h5/'
 
-filename = archivos_clientes[0]
-print('filename: ' + filename)
-client_file = pd.read_csv(
-    INPUT_DATA_PATH + filename + '.' + INPUT_EXTENSION,
-    sep='\|\|')
-    # ,
-    # encoding='latin-1')
-
 for filename in archivos_clientes:
     print('filename: ' + filename)
     client_file = pd.read_csv(
         INPUT_DATA_PATH + filename + '.' + INPUT_EXTENSION,
         sep='\|\|',
-        encoding='latin-1')
-
+        encoding='utf-8')
     print(client_file.shape)
     client_file.to_hdf(OUTPUT_DATA_PATH + filename + "." + OUT_EXTENSION,
                  key='df',
                  mode='w')
-
-
-# data_cupo_vigente_raw = pd.read_hdf('/home/pandrade/use-case-azul/03-Data/data_cupo_vigente_raw.h5', 'df')
