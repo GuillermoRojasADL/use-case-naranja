27522ba5cd73110831a42f8c859af1cb1aa6c0de
pablo-andrade <pablo_andrade@mckinsey.com>
2019-06-28 14:32:16 -0500

    comment to read h5

--- 02-Python-code/00-ReadTextFiles.py
+++ 02-Python-code/00-ReadTextFiles.py
@@ -1,15 +1,20 @@
 import pandas as pd
+pd.options.mode.chained_assignment = None  # Disable annoying pandas warning
+
+# archivos_clientes = \
+#     [
+#     # 'sbgr1_ps_bc',
+#     'sbgr1_ps_bo_role',
+#     # 'sbgr1_ps_por_habdata_tbl',
+#     # 'sbgr1_ps_por_salotr_tbl',
+#     # 'sbgr1_ps_aa_rd_person',
+#     'sbgr1_ps_bo_cm',
+#     'sbgr1_ps_cm',
+#     #'sbgr1_ps_por_inf_com_tbl',
+#     #'sbgr1_ps_rd_person']
 
 archivos_clientes = \
-    ['sbgr1_ps_bc',
-    'sbgr1_ps_bo_role',
-    'sbgr1_ps_por_habdata_tbl',
-    'sbgr1_ps_por_salotr_tbl',
-    'sbgr1_ps_aa_rd_person',
-    'sbgr1_ps_bo_cm',
-    'sbgr1_ps_cm',
-    'sbgr1_ps_por_inf_com_tbl',
-    'sbgr1_ps_rd_person']
+    ['sbgr1_ps_bo_role','sbgr1_ps_bo_cm','sbgr1_ps_cm']
 
 INPUT_EXTENSION = 'txt'
 OUT_EXTENSION = 'h5'
@@ -17,12 +22,20 @@ OUT_EXTENSION = 'h5'
 INPUT_DATA_PATH = '/mnt/s3-refined-porvenir/Clientes/'
 OUTPUT_DATA_PATH = '/mnt/s3-refined-porvenir/Clientes/h5/'
 
+filename = archivos_clientes[0]
+print('filename: ' + filename)
+client_file = pd.read_csv(
+    INPUT_DATA_PATH + filename + '.' + INPUT_EXTENSION,
+    sep='\|\|',
+    encoding='latin-1')
+
 for filename in archivos_clientes:
     print('filename: ' + filename)
     client_file = pd.read_csv(
         INPUT_DATA_PATH + filename + '.' + INPUT_EXTENSION,
         sep='\|\|',
         encoding='latin-1')
+
     print(client_file.shape)
     client_file.to_hdf(OUTPUT_DATA_PATH + filename + "." + OUT_EXTENSION,
                  key='df',
