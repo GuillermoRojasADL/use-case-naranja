3191c5a478745730a06518d8bad6f4530d703363
Guillermo <fabian.rojas@avaldigitallabs.com>
2019-09-18 12:05:41 -0500

    MDT con Pensiones Voluntarias

---
+++ 02-Python-code/MDT_model/MDT_PV.py
@@ -0,0 +1,86 @@
+import os
+import pandas as pd
+import pickle
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+###################
+# transform text files to h5 files
+INPUT_EXTENSION = '.pkl'
+
+INPUT_DATA_PATH = '/mnt/work/mdt/'
+CES_PV_PREF_PATH = '/mnt/work/datasets/preMDT_cesantias_pi_def//'
+FILE_NAME = '20190827-final-master_table_churn-5cats_dummies-rem-leakage'
+#OUTPUT_DATA_PATH = '/home/pandrade/'
+print('loading data from' + INPUT_DATA_PATH + FILE_NAME + INPUT_EXTENSION)
+ini=time.time()
+
+year_month =\
+    pd.DataFrame(pd.date_range(start = '2017-05-01', end='2019-06-01',
+                               freq='MS').map(lambda x: np.str(x.year) + "" + np.str(x.month).rjust(2,'0')))
+year_month.columns = ['YearMonth']
+
+
+with open(INPUT_DATA_PATH + FILE_NAME + INPUT_EXTENSION, 'rb') as input:
+    dataset = pickle.load(file=input)
+
+#Archivo de cesantias
+file = 'cesantias_preMDT.snappy.parquet'
+ces = pd.read_parquet(os.path.join(CES_PV_PREF_PATH,file))
+
+file = 'pv_preMDT.snappy.parquet'
+pv = pd.read_parquet(os.path.join(CES_PV_PREF_PATH,file))
+
+## Join  con MDT
+pv = pv.rename(columns = {'SALDO_TOTAL':'SALDO_TOTAL_PV'})
+
+
+pv['FEC_APERTURA'] = pv['FEC_APERTURA'].replace('"', np.nan)
+pv['FEC_APERTURA'] = pd.to_datetime(pv['FEC_APERTURA'].apply(str), format='%d-%b-%y')
+
+
+pv.drop(columns='COD_CUENTA_ANONI', inplace=True)
+
+df = dataset[['IDENTIFICADOR', 'YearMonth']]
+
+
+
+join_pv = pd.merge(df,
+                     pv,
+                     how='left',
+                     left_on=['IDENTIFICADOR'],
+                     right_on = ['IDENTIFICADOR'])
+
+
+
+join_pv['YearMonth'] = pd.to_datetime(join_pv['YearMonth'].apply(str)+'01', format='%Y%m%d', errors='coerce')
+join_pv['SALDO_FINAL_PV'] = np.where((join_pv['YearMonth']<= join_pv['FEC_ULT_APORTE']) &
+                               (join_pv['YearMonth']>= join_pv['FEC_APERTURA']),
+                                     join_pv['SALDO_TOTAL_PV'],
+                                     0)
+
+join_pv['TIENE_PV'] = np.where((join_pv['YearMonth']>= join_pv['FEC_APERTURA']), 1, 0 )
+
+
+
+join_pv = join_pv.drop_duplicates()
+
+join_pv.shape
+join_pv['YearMonth'] = 100*join_pv['YearMonth'].dt.year + join_pv['YearMonth'].dt.month
+join_pv['YearMonth'] = join_pv['YearMonth'].astype(str)
+
+
+
+join_pv_dataset \
+    = pd.merge(dataset,
+                     join_pv,
+                     how='left',
+                     left_on=['IDENTIFICADOR','YearMonth'],
+                     right_on = ['IDENTIFICADOR','YearMonth'])
+
+
+
+join_pv_dataset.to_parquet(os.path.join(INPUT_DATA_PATH, '20190827-final-master_table_churn-5cats_dummies-rem-leakage-PI' + '.snappy.parquet'),compression='snappy')
+
---
+++ 02-Python-code/MDT_model/pre_MDT_PV.py
@@ -0,0 +1,79 @@
+import os
+
+import pandas as pd
+
+#########################################################
+
+input_extension = '.snappy.parquet'
+output_path = '/mnt/work/datasets/preMDT_cesantias_pi_def//'
+
+# cuenta: sbgr6_fo_cuentas.txt
+file = 'sbgr6_fo_cuentas.txt'
+pi_cta = pd.read_parquet(os.path.join('/mnt/work/datasets/SG6_parquet', file + '.snappy.parquet'))
+
+file = 'sbgr6_af_datos_basicos.txt'
+pi_id = pd.read_csv(os.path.join('/mnt/s3-refined-porvenir/SG6/main', file),  sep='\|\|', encoding='latin1',engine='python')
+
+file = 'sbgr6_enca_solicitud.txt'
+connect = pd.read_parquet(os.path.join('/mnt/work/datasets/SG6_parquet', file + '.snappy.parquet'))
+
+file = 'sbgr6_fo_aportes.txt'
+fo_aportes = pd.read_parquet(os.path.join('/mnt/work/datasets/SG6_parquet', file+ '.snappy.parquet'))
+
+#fo_aportes.shape # (42932448, 27)
+#connect.shape # (271050, 6)
+#pi_cta.shape # (304907, 12)
+#pi_id.shape # (266264, 9)
+
+connect['NUM_FORMULARIO'] = connect['NUM_FORMULARIO'].astype(str)
+
+df_join_ini = pd.merge(pi_id,
+                     connect,
+                     how='inner',
+                     left_on=['NUM_FORMULARIO'],
+                     right_on = ['NUM_FORMULARIO'])
+
+#Filtro de activos en PI
+# sel_cols = ['COD_CUENTA_ANONI','IDENTIFICADOR_HASH','FECHA_ACTIVACION']
+cond = (pi_cta['EST_CUENTA']=='A')
+pi_cta_active = pi_cta[cond]
+
+df_final = pd.merge(pi_cta_active ,
+                     df_join_ini,
+                     how='inner',
+                     left_on=['COD_CUENTA_ANONI'],
+                     right_on = ['COD_CUENTA'])
+
+
+#creación yearmonth
+df_final['FEC_ULT_APORTE'] = pd.to_datetime(df_final['FEC_ULT_APORTE'].apply(str), format='%d-%b-%y', errors='coerce')
+df_final['YearMonth'] = 100*df_final['FEC_ULT_APORTE'].dt.year + df_final['FEC_ULT_APORTE'].dt.month
+
+#CREACIÓN DE VARIABLE DE SALDO TOTAL
+fo_aportes['FECHA_CONSIGNACION'] = pd.to_datetime(fo_aportes['FECHA_CONSIGNACION'].apply(str), format='%d-%b-%y', errors='coerce')
+fo_aportes['YearMonth'] = 100*fo_aportes['FECHA_CONSIGNACION'].dt.year + fo_aportes['FECHA_CONSIGNACION'].dt.month
+
+fo_aportes['SALDO_TOTAL'] = fo_aportes['SALDO_CAPITAL_AFL'] +fo_aportes['SALDO_CAPITAL_PAT'] + \
+                fo_aportes['SALDO_RET_CONTINGENTE_AFL'] + fo_aportes['SALDO_RET_CONTINGENTE_PAT'] + \
+                 fo_aportes['SALDO_RENDIMIENTOS_AFL'] + fo_aportes['SALDO_RENDIMIENTOS_PAT']
+
+#ultimos 7 años de aportes y cuentas activas
+fo_aportes_filter = fo_aportes[fo_aportes['YearMonth'] > 201204]
+
+aportes_grouped = fo_aportes_filter[['COD_CUENTA','YearMonth','SALDO_TOTAL']].\
+                groupby(['COD_CUENTA','YearMonth'], as_index=False, sort = False).max()
+
+
+
+df_final_saldo = pd.merge(df_final ,
+                     aportes_grouped,
+                     how='inner',
+                     left_on=['COD_CUENTA_ANONI','YearMonth'],
+                     right_on = ['COD_CUENTA','YearMonth'])
+
+sel_cols = ['COD_CUENTA_ANONI','IDENTIFICADOR','FEC_ULT_APORTE','FEC_APERTURA','SALDO_TOTAL']
+df_to_write = df_final_saldo[sel_cols]
+
+
+name_file = 'pv_preMDT'
+df_to_write.to_parquet(os.path.join(output_path, name_file + '.snappy.parquet'), compression='snappy')
